a19a855fbffcbf04a678dddc82c815901567ed53
joseegido <joseegido.alu@iespacomolla.es>
2024-06-04 12:36:25 +0200

    Editar desarrollador

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@ Route::get('/', function () {
 })->name('dashboard');
 
 Route::resource('videojuegos', VideojuegoController::class);
-Route::resource('desarrolladores', DesarrolladorController::class);
+// Route::resource('desarrolladores', DesarrolladorController::class);
 Route::resource('resenas', ResenaController::class);
 Route::resource('etiquetas', EtiquetaController::class);
+// Muestra una lista de los recursos.
+Route::get('desarrolladores', [DesarrolladorController::class, 'index'])->name('desarrolladores.index');
+
+// Muestra el formulario para crear un nuevo recurso.
+Route::get('desarrolladores/create', [DesarrolladorController::class, 'create'])->name('desarrolladores.create');
+
+// Almacena un recurso recién creado en el almacenamiento.
+Route::post('desarrolladores', [DesarrolladorController::class, 'store'])->name('desarrolladores.store');
+
+// Muestra el formulario para editar el recurso especificado.
+Route::get('desarrolladores/{desarrollador}/edit', [DesarrolladorController::class, 'edit'])->name('desarrolladores.edit');
+
+// Actualiza el recurso especificado en el almacenamiento.
+Route::put('desarrolladores/{desarrollador}', [DesarrolladorController::class, 'update'])->name('desarrolladores.update');
+
+// Elimina el recurso especificado del almacenamiento.
+Route::delete('desarrolladores/{desarrollador}', [DesarrolladorController::class, 'destroy'])->name('desarrolladores.destroy');
